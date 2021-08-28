7b528e733bf8f98caebbe27f3e245ca3ca4190e6
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-08-27 20:04:11 -0700

    Update unqcharinstr.py

--- unqcharinstr.py
+++ unqcharinstr.py
@@ -3,7 +3,7 @@ class Solution:
         prevInv = set()  #using a set, initializing and creating the set for the function to test letter entries in a string of s
         
         for i, letter in enumerate(s): ##saying that for i, enumerate letter in s, since the iterable is a sequence,  if the letter in s returns a tuple containing a count (from start which defaults to 0) and the values obtained from iterating will be over iterable for str of s or not
-            if letter not in prevInv: ## arguement to test if letter is not in either s or prevInv
+            if letter not in prevInv: ## a sequence to test if letter is not in either s or prevInv
                 if letter not in s[i+1:]: 
                     return i
                 else:
