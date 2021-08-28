5f8e4ebe52e6c4c3efb58439dfafa98435587bdb
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-08-27 19:48:56 -0700

    Update unqcharinstr.py

--- unqcharinstr.py
+++ unqcharinstr.py
@@ -2,7 +2,7 @@ class Solution:
     def firstUniqChar(self, s: str) -> int:
         prevInv = set()  #using a set, initializing and creating the set for the function to test letter entries in a string of s
         
-        for i, letter in enumerate(s): ##checking in i the letter, since the iterable is a sequence,  if the letter in s returns a tuple containing a count (from start which defaults to 0) and the values obtained from iterating will be over iterable for str of s or not
+        for i, letter in enumerate(s): ##saying that for i, enumerate letter in s, since the iterable is a sequence,  if the letter in s returns a tuple containing a count (from start which defaults to 0) and the values obtained from iterating will be over iterable for str of s or not
             if letter not in prevInv:
                 if letter not in s[i+1:]: 
                     return i
