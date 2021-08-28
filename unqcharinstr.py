6496b0c508b0323990b1c6e3686425ac332da3ae
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-08-27 19:36:39 -0700

    Update unqcharinstr.py

--- unqcharinstr.py
+++ unqcharinstr.py
@@ -1,13 +1,13 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        prevInv = set()
+        prevInv = set()  #using a set, initializing and creating the set for the function
         
-        for i, letter in enumerate(s):
+        for i, letter in enumerate(s): ##checking in i the letter, since the iterable is a sequence,  if the letter in s returns a tuple containing a count (from start which defaults to 0) and the values obtained from iterating will set over iterable for str of s or not
             if letter not in prevInv:
-                if letter not in s[i+1:]:
+                if letter not in s[i+1:]: 
                     return i
                 else:
-                    prevInv.add(letter)
+                    prevInv.add(letter) ## 
         return -1
 		
 		
